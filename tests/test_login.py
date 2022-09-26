857ca798b3511d267d7ed8d48f29fa4be76250c2
VitaliiStasiv <stasivvitalii@gmail.com>
2022-09-26 20:23:12 +0300

    fixed problem with test,need to deal with assert

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,10 @@
 import time
 
 
+def test_login(login_page):
+    time.sleep(2)
+
+
 def test_false_login(login_page):
     login_page.open_login_form().fill_confirm_login_form().check_if_login_failed()
     time.sleep(2)
